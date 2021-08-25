eaedef3d069b9d45eeda120aa3acd0c8a08ebbff
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2021-08-26 01:31:33 +0530

    2nd Commit

--- server.js
+++ server.js
@@ -28,24 +28,10 @@ mongoose
 
 app.use(cors());
 
-//Define Routes
-//app.use("url", require("path of the file you want to render"))
-
-// app.get("/", (req, res) => res.send("API RUNNING"));
 
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/users", require("./routes/api/users"));
-
-// Serve static assests, if in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`SERVER WORKING ON ${port}`));
